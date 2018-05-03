6b8f6ca5403895be110a64a4b75e32e5f8761165
Abadi Kurniawan <abadi.kurniawan@gmail.com>
2018-05-03 10:24:26 -0500

    Insert data to mongodb.

--- search/main.go
+++ search/main.go
@@ -5,8 +5,20 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/globalsign/mgo"
+	"github.com/globalsign/mgo/bson"
 	"github.com/gorilla/websocket"
 )
+
+var session *mgo.Session
+
+func init() {
+	var err error
+	session, err = mgo.Dial("mongodb://search_db:27017")
+	if err != nil {
+		fmt.Printf("Error initializing mongodb connection: %v\n", err)
+	}
+}
 
 func main() {
 	http.HandleFunc("/", handler)
@@ -24,6 +36,7 @@ func checkOrigin(r *http.Request) bool {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	db := session.DB("go-search")
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
@@ -36,10 +49,26 @@ func handler(w http.ResponseWriter, r *http.Request) {
 			log.Println(err)
 			return
 		}
-		fmt.Println(string(p))
+
+		insertMessage(db, Message{bson.NewObjectId(), string(p)})
+
 		if err := conn.WriteMessage(messageType, p); err != nil {
 			log.Println(err)
 			return
 		}
+	}
+}
+
+type Message struct {
+	ID    bson.ObjectId `json:"_id" bson:"_id"`
+	Value string        `json:"value" bson:"value"`
+}
+
+func insertMessage(db *mgo.Database, msg Message) {
+	fmt.Printf("Inserting %v to db\n", msg.Value)
+	col := db.C("message")
+	err := col.Insert(msg)
+	if err != nil {
+		fmt.Println(err.Error())
 	}
 }
